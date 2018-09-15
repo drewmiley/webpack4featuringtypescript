a56564b18d1a468a21a30e7bbb05329fb536642b
Drew Miley <drewmiley92@gmail.com>
2018-09-15 22:50:05 +0100

    Typescripts actions

--- src/ducks/actions.ts
+++ src/ducks/actions.ts
@@ -1,6 +1,6 @@
 import * as actiontypes from './actiontypes';
 
-export function setNumber(number) {
+export function setNumber(number: Number) {
     return {
         type: actiontypes.SET_NUMBER,
         number
