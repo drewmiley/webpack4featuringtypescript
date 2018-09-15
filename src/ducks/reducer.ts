e1efd1bfea67ece66582b8db011910764131662e
Drew Miley <drewmiley92@gmail.com>
2018-09-16 00:36:13 +0100

    Changes default value in function

--- src/ducks/reducer.ts
+++ src/ducks/reducer.ts
@@ -4,7 +4,7 @@ function setNumber(state: Object, number: Number) {
     return Object.assign({}, { number });
 }
 
-export default function(state = {}, action: any) {
+export default function(state: Object | {}, action: any) {
     switch (action.type) {
         case actiontypes.SET_NUMBER:
             return setNumber(state, action.number);
